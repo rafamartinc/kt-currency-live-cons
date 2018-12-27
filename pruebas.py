1b7289262ae549eab5688adb1f466d257fcf3966
Ruben S <rbn7sainz@gmail.com>
2018-12-27 15:00:32 +0100

    cambio hourly por live

--- pruebas.py
+++ pruebas.py
@@ -5,29 +5,28 @@ from influxdb import InfluxDBClient
 
 def main(host, port, dbname):
 
-    consumer_hour = KafkaConsumer(kt_hourly_currencies, auto_offset_reset='earliest',
-                                 bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
-    for msg in consumer_hour:
-        json_body_AggregatedPoints = [
-            {"measurement": "AggregatedPoints",
-             'time': consumer_hour['timestamp'],
-             "tags": {
-                 'currency': consumer_hour['currency'],
-                 'reference_currency': consumer_hour['reference_currency'],
-                 'API': consumer_hour['api']
-             },
-             "fields": {
-                 'value': consumer_hour['value'],
-             }
-             }
+    consumer = KafkaConsumer(kt_currencies, auto_offset_reset='earliest',
+                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+    for msg in consumer:
+        json_body_SinglePoints = [
+                {"measurement": "SinglePoints",
+                'time': consumer['timestamp'],
+                "tags":{
+                    'currency': consumer['currency'],
+                    'reference_currency': consumer['reference_currency'],
+                    'API': consumer['api']
+                },
+                "fields":{
+                    'value': consumer['value'],
+                    }
+                }
         ]
-
         client = InfluxDBClient(host, port)
         print("Create database: " + dbname)
         client.create_database(dbname)
         print("Create a retention policy")
-        client.create_retention_policy('awesome_policy',  '30d', replication='1', database=dbname, default=True)
-        client.write_points(json_body_AggregatedPoints,database=dbname,protocol='json')
+        client.create_retention_policy('awesome_policy', '30d', replication='1', database=dbname, default=True)
+        client.write_points(json_body_SinglePoints, database=dbname, protocol='json')
 
 def parse_args():
     """Parse the args."""
@@ -46,4 +45,5 @@ if __name__ == '__main__':
     args = parse_args()
     print(args.host, args.port, args.dbname)
     main(host=args.host, port=args.port, dbname=args.dbname)
+
 
